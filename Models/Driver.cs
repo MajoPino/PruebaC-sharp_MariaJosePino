0a80995b1da38b204df752c5c39f59962be10bb9
MajoPino <majo12pino@gmail.com>
2024-08-02 07:19:26 -0500

    feat: Creation of class Driver

---
+++ Models/Driver.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PruebaC_sharp_MariaJosePino.Models
+{
+    public class Driver : User
+    {
+        public string? LicenseNumber { get; set; }
+        public string? LicenseCategory { get; set; }
+        public int DrivingExperience { get; set; }
+
+        public Driver(string name, string lastName, string typeDocument, string identificationNumber, DateOnly birthDate, string email, string phoneNumber, string address, string licenseNumber, string licenseCategory, int drivingExperience) : base(name, lastName, typeDocument, identificationNumber, birthDate, email, phoneNumber, address)
+        {
+            LicenseNumber = licenseNumber;
+            LicenseCategory = licenseCategory;
+            DrivingExperience = drivingExperience;
+        }
+
+        public void UpdateLicenseCategory(string newCategory)
+        {
+            LicenseCategory = newCategory;
+        }
+
+        public void AddExperience(int years)
+        {
+            DrivingExperience = years;
+        }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -3,10 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace PruebaC_sharp_MariaJosePino.Models
-{
+namespace PruebaC_sharp_MariaJosePino.Models;
+
     public class User
     {
-        
+        protected Guid Id { get; set; }
+        protected string? Name { get; set; }
+        protected string? LastName { get; set; }
+        protected string? TypeDocument { get; set; }
+        protected string? IdentificationNumber { get; set; }
+        protected DateOnly BirthDate { get; set; }
+        protected string? Email { get; set; }
+        protected string? PhoneNumber { get; set; }
+        protected string? Address { get; set; }
+
+        protected User(string name, string lastName, string typeDocument, string identificationNumber, DateOnly birthDate, string email, string phoneNumber, string address)
+        {
+            Id = new Guid();
+            Name = name;
+            LastName = lastName;
+            TypeDocument = typeDocument;
+            IdentificationNumber = identificationNumber;
+            BirthDate = birthDate;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            Address = address;
+        }
+
+        protected void ShowDetails()
+        {
+            Console.WriteLine(@$"Full Name: {Name} {LastName}
+Identification Number: {TypeDocument} {IdentificationNumber}
+Birth Date: {BirthDate.Year}
+Email: {Email}
+Phone Number: {PhoneNumber}
+Address: {Address}");
+        }
+
+        protected int CalculateAge()
+        {
+            int birthDateYear = BirthDate.Year;
+            int thisYear = DateTime.Now.Year;
+
+            int age = birthDateYear - thisYear;
+
+            return age;
+
+        }
+
+        protected void ShowAge()
+        {
+            int age = CalculateAge();
+            Console.WriteLine(@$"The user {Name} {LastName} is {age} years old.");
+        }
     }
-}
